5c6559dbf3b15ea521e36b508f9da25fa8f99331
George-Kalash <egor122t53@gmail.com>
2025-07-15 12:56:59 -0400

    checkup commit

--- GeorgeKalPortfolio/src/components/header.tsx
+++ GeorgeKalPortfolio/src/components/header.tsx
@@ -3,12 +3,16 @@ export default function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 ">
+      <header className="fixed flex top-0 left-0 right-0 z-50 justify-between">
           <div className="left-side">
-            <div id="name" className="text-xl text-black inline font-bold hover:text-red-700 active:text-green-700">George K</div>
+            <div id="name" className="m-[10px] text-xl text-black font-bold hover:text-red-700 active:text-green-700">George K</div>
           </div>
-          <div className="right-side">
-
+          <div className="right-side flex">
+            <nav>Home</nav>
+            <nav>Experience</nav>
+            <nav>Projects</nav>
+            <nav>Connect</nav>
+            <div className="theme_change_icon"></div>
           </div>
       </header>
     </>
