08ab76c69b9146c3e28b6b43e252d1d6aa1bdf97
George-Kalash <egor122t53@gmail.com>
2025-07-20 00:11:17 -0400

    marquee

--- GeorgeKalPortfolio/src/components/rollingText.tsx
+++ GeorgeKalPortfolio/src/components/rollingText.tsx
@@ -4,12 +4,13 @@ import Marquee from "react-fast-marquee";
 export default function RollingText() {
   return (
     <div className="min-w-2xs max-w-3/4 m-auto overflow-hidden">
-      <div className="flex overflow-hidden perspective-[600px] h-100 self-center-safe justify-self-center-safe mt-52">
+      <div className="flex overflow-hidden perspective-[700px] h-100 self-center-safe justify-self-center-safe mt-52">
         <Marquee
-          speed={200}
-          className="ml-10 m-20 text-[130px] font-semibold text-center text-white"
-          style={{transform: "rotateY(-10deg) skewX(1deg) skewY(-1deg) translateZ(0px) perspective(100vw) rotateY(18deg)"}}
+          speed={600}
+          className="ml-10 m-20 text-[100px] font-semibold text-center text-white"
+          style={{transform: "rotateY(30deg)"}}
         >
+        <h1 className="opacity-0"> Lorem </h1>
         <h1 className="opacity-0"> Lorem </h1>
         <h1>  Ideas Are Just Ideas Without Someone to Make Them a Reality!   </h1>
         <h1 className="opacity-0"> Lorem </h1>
